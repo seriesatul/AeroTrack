7d3c5b2acb02bba28897b501235e596cb1637868
seriesatul <atulsingh04895@gmail.com>
2025-11-18 21:22:43 +0530

    feat: prepare backend for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 // backend/config/db.js
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
     try {
         // IMPORTANT: Replace this with your actual MongoDB connection string!
-        const MONGO_URI = 'mongodb+srv://atulsingh04895_db_user:NaQewJonwEPThtSj@cluster0.c3mz75w.mongodb.net/?appName=Cluster0'; 
-
+        const MONGO_URI = process.env.MONGO_URI; 
         const conn = await mongoose.connect(MONGO_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
